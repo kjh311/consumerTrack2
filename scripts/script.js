6f54c785ce5667b0bf763428f07b236cccba46a6
Kevin <kjh311@gmail.com>
2018-09-26 08:43:26 -0700

    form validation works

--- scripts/script.js
+++ scripts/script.js
@@ -20,6 +20,7 @@ function validateNameForm() {
 	var emailWrapper = document.getElementById("email-form-div");
 	var errorText = document.getElementById("age-error-text");
 
+
 	// console.log("Year born on was " + bornYear);
 	// console.log("Month born on was " + bornMonth);
 	// console.log("Day born on was " + bornDay);
@@ -54,22 +55,72 @@ function validateEmailForm(){
 	var emailAddress = document.getElementById('email-input').value;
 	var acountExistsWarning = document.getElementById("email-error-div");
 
+	
+
 	if(emailAddress === "testemail@test.com"){
 		acountExistsWarning.classList.remove("invisible");
 		emailInput.style.borderColor = "#df4e60";
 		return false;
 	}
+
+
 }
 
-function checkDuplicateEmail(){
+function addRedBorderEmail(){
+	var emailInput = document.getElementById('email-input');
+	    emailInput.style.borderColor = "#df4e60"
+}
 
+function addRedBorderName(){
+	var nameInput = document.getElementById('first-name-input');
+	    nameInput.style.borderColor = "#df4e60"
+}
+
+function addRedBorderDayDrop(){
+	var dayDropSelect = document.getElementById('day-drop');
+	    dayDropSelect.style.borderColor = "#df4e60"
+}
+
+function addRedBorderMonthDrop(){
+	var monthDropSelect = document.getElementById('month-drop');
+	    monthDropSelect.style.borderColor = "#df4e60"
+}
+
+function addRedBorderYearDrop(){
+	var yearDropSelect = document.getElementById('year-drop');
+	    yearDropSelect.style.borderColor = "#df4e60"
+}
+
+// function removeEmailInputBorder(){
+
+// }
+
+function removeEmailInputBorder(){
 	var emailInput = document.getElementById('email-input');
 	var acountExistsWarning = document.getElementById("email-error-div");
-
 	emailInput.style.borderColor = "#e4e4e4"
 
 	if(!acountExistsWarning.classList.contains("invisible")){
 		acountExistsWarning.className += " invisible "
 	}
+}
 
+function removeNameInputBorder(){
+	var nameInput = document.getElementById('first-name-input');
+	nameInput.style.borderColor = "#e4e4e4"
+}
+
+function removeMonthDropRedBorder(){
+	var monthDrop = document.getElementById('month-drop');
+	monthDrop.style.borderColor = "#e4e4e4"
+}
+
+function removeYearDropRedBorder(){
+	var yearDrop = document.getElementById('year-drop');
+	yearDrop.style.borderColor = "#e4e4e4"
+}
+
+function removeDayDropRedBorder(){
+	var dayDrop = document.getElementById('day-drop');
+	dayDrop.style.borderColor = "#e4e4e4"
 }
